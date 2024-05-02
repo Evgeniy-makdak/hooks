4557ba110edf2293eb36f8e690d7a6b3fcb38ecf
Evgeniy-makdak <makdak23@mail.ru>
2024-05-02 22:53:00 +0300

    create hook useEffect and created a controlled input

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,17 +1,24 @@
-import { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 
 function Counter() {
-
     const [count, setCount] = useState(0);
+    const [text, setText] = useState('Input anything text');
+
+    // Эмуляция запроса на сервер при изменении счетчика - пример использования хука useEffect.
+    // Применяется, когда нужно выполнить побочные эффекты (в данном случае выводим в алерт надпись, но, по факту, может быть запрос на 
+    // сервер при изменении состояния "count" - об этом говорит аргумент [count]).   
+    // useEffect(() => {
+    //     alert(`Sending request to server with count: ${count}`);
+    // }, [count]);
 
     function up() {
-        setCount(prevCount => prevCount + 1)
+        setCount(prevCount => prevCount + 1);
     }
 
     function down() {
-        setCount(prevCount => prevCount - 1)
+        setCount(prevCount => prevCount - 1);
     }
+
 
     return (
         <div className="counter">
@@ -20,6 +27,8 @@ function Counter() {
                 <button className="btnCounter" onClick={up}>click+</button>
                 <button className="btnCounter" onClick={down}>click-</button>
             </div>
+            <div className="mainTitle">{text}</div>
+            <input value={text} onChange={(event) => setText(event.target.value)} className="input" type="text" />
         </div>
     );
 }
